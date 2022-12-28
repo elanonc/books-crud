7167b386ae11f7968c831c823b2c83b8233de548
Elano N. Caitano <nunes.elano14@gmail.com>
2022-12-27 23:28:47 -0300

    Implementação do CRUD

--- src/modules/book/book.controller.ts
+++ src/modules/book/book.controller.ts
@@ -1,7 +1,36 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { BookDTO } from './book.dto';
 import { BookService } from './book.service';
 
 @Controller('book')
 export class BookController {
   constructor(private readonly bookService: BookService) {}
+
+  @Post()
+  async create(@Body() data: BookDTO) {
+    return this.bookService.create(data);
+  }
+
+  @Get()
+  async findAll() {
+    return this.bookService.findAll();
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() data: BookDTO) {
+    return this.bookService.update(id, data);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    return this.bookService.delete(id);
+  }
 }
--- src/modules/book/book.module.ts
+++ src/modules/book/book.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BookService } from './book.service';
 import { BookController } from './book.controller';
+import { PrismaService } from 'src/database/prisma-service';
 
 @Module({
   controllers: [BookController],
-  providers: [BookService],
+  providers: [BookService, PrismaService],
 })
 export class BookModule {}
